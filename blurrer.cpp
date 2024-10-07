a8e76312d5d85683856d05fee89b539a03b08f41
Khue Nguyen <Z5483Y@gmail.com>
2024-10-07 18:59:11 -0400

    feat: add convolver

--- blurrer.cpp
+++ blurrer.cpp
@@ -1,6 +1,7 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <string>
-#include <boost/program_options.hpp>
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -8,46 +9,103 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-int
-main(int argc, char *argv[])
-{
-	boost::program_options::options_description desc("Allowed options");
-	desc.add_options()
-		("input,i", boost::program_options::value<std::string>(), "set input file name")
-		("output,o", boost::program_options::value<std::string>(), "set output file name")
-		("algo,a", boost::program_options::value<std::string>(), "set algorithm")
-		("strength,s", boost::program_options::value<std::string>(), "set blur strength")
-		("help,h", "display usage message");
+std::vector<std::vector<float>>
+pad_image(const std::vector<std::vector<float>> &image, int pad_h, int pad_w) {
+  int Hi = image.size();
+  int Wi = image[0].size();
+  int padded_Hi = Hi + 2 * pad_h;
+  int padded_Wi = Wi + 2 * pad_w;
 
-	if (argc == 1) {
-		std::cout << desc << "\n";
-	}
+  std::vector<std::vector<float>> padded(padded_Hi,
+                                         std::vector<float>(padded_Wi));
 
-	boost::program_options::variables_map vm;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-	boost::program_options::notify(vm);
+  for (int i = 0; i < padded_Hi; ++i) {
+    for (int j = 0; j < padded_Wi; ++j) {
+      int src_i = std::clamp(i - pad_h, 0, Hi - 1);
+      int src_j = std::clamp(j - pad_w, 0, Wi - 1);
+      padded[i][j] = image[src_i][src_j];
+    }
+  }
 
-	int width, height, channels;
-	unsigned char *image;
-	std::string image_name;
+  return padded;
+}
 
-	if (vm.count("input")) {
-		image_name = vm["input"].as<std::string>();
-		image = stbi_load(image_name.c_str(), &width, &height, &channels, 0);
-	}
+std::vector<std::vector<float>>
+conv(const std::vector<std::vector<float>> &image,
+     const std::vector<std::vector<float>> &kernel) {
+  int Hi = image.size();
+  int Wi = image[0].size();
+  int Hk = kernel.size();
+  int Wk = kernel[0].size();
 
-	if (vm.count("help")) {
-		std::cout << desc << "\n";
-		return 0;
-	}
+  std::vector<std::vector<float>> out(Hi, std::vector<float>(Wi, 0));
 
+  int pad_h = Hk / 2;
+  int pad_w = Wk / 2;
+  std::vector<std::vector<float>> padded = pad_image(image, pad_h, pad_w);
 
-	if (image == nullptr) {
-		std::cerr << "Error: could not load image: " << image_name << std::endl;
-		return 1;
-	}
+  for (int image_h = 0; image_h < Hi; ++image_h) {
+    for (int image_w = 0; image_w < Wi; ++image_w) {
+      float sum = 0;
+      for (int kh = 0; kh < Hk; ++kh) {
+        for (int kw = 0; kw < Wk; ++kw) {
+          sum += kernel[kh][kw] * padded[image_h + kh][image_w + kw];
+        }
+      }
+      out[image_h][image_w] = sum;
+    }
+  }
 
-	stbi_image_free(image);
+  return out;
+}
 
-	return 0;
+int main(int argc, char *argv[]) {
+  boost::program_options::options_description desc("Allowed options");
+  desc.add_options()("input,i", boost::program_options::value<std::string>(),
+                     "set input file name")(
+      "output,o", boost::program_options::value<std::string>(),
+      "set output file name")(
+      "algo,a", boost::program_options::value<std::string>(), "set algorithm")(
+      "strength,s", boost::program_options::value<std::string>(),
+      "set blur strength")("help,h", "display usage message");
+
+  if (argc == 1) {
+    std::cout << desc << "\n";
+    return 0;
+  }
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(
+      boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+
+  int width, height, channels;
+  unsigned char *image_data;
+  std::string image_name;
+
+  if (vm.count("input")) {
+    image_name = vm["input"].as<std::string>();
+    image_data = stbi_load(image_name.c_str(), &width, &height, &channels, 0);
+  }
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 0;
+  }
+
+  if (image_data == nullptr) {
+    std::cerr << "Error: could not load image: " << image_name << std::endl;
+    return 1;
+  }
+
+  std::vector<std::vector<float>> image(height, std::vector<float>(width));
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; ++j) {
+      image[i][j] = static_cast<float>(image_data[(i * width + j) * channels]);
+    }
+  }
+
+  stbi_image_free(image_data);
+
+  return 0;
 }
