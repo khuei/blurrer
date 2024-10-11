c51607eafaa252ee6b1e1924c81b2b7a5a93794d
Khue Nguyen <Z5483Y@gmail.com>
2024-10-10 21:34:19 -0400

    refactor: remove redundancy of generating a 2D kernel before a 3D one

--- blurrer.cpp
+++ blurrer.cpp
@@ -70,35 +70,25 @@ conv(const std::vector<std::vector<std::vector<float>>> &image,
 }
 
 std::vector<std::vector<std::vector<float>>> gaussian_kernel(int size, int channels) {
-  std::vector<std::vector<float>> base_kernel(size, std::vector<float>(size, 0));
-
-  double sigma = ((double)size / 2 > 1) ? (double)size / 2 : 1;
-  int k = (size - 1) / 2;
-
-  for (int i = 0; i < size; ++i) {
-    for (int j = 0; j < size; ++j) {
-      double exponent = -((std::pow(i - k, 2) + std::pow(j - k, 2)) / (2 * sigma * sigma));
-      base_kernel[i][j] = (1 / (2 * M_PI * sigma * sigma)) * std::exp(exponent);
-    }
-  }
-
   std::vector<std::vector<std::vector<float>>> kernel(
       size,
       std::vector<std::vector<float>>(size, std::vector<float>(channels, 0))
   );
 
-  for (int i = 0; i < size; ++i) {
-    for (int j = 0; j < size; ++j) {
-      for (int c = 0; c < channels; ++c) {
-        kernel[i][j][c] = base_kernel[i][j]; // Same value for each channel
-      }
-    }
-  }
+  double sigma = ((double)size / 2 > 1) ? (double)size / 2 : 1;
+  int k = (size - 1) / 2;
 
   double sum = 0.0;
+
   for (int i = 0; i < size; ++i) {
     for (int j = 0; j < size; ++j) {
-      sum += kernel[i][j][0];
+      double exponent = -((std::pow(i - k, 2) + std::pow(j - k, 2)) / (2 * sigma * sigma));
+      float value = (1 / (2 * M_PI * sigma * sigma)) * std::exp(exponent);
+
+      for (int c = 0; c < channels; ++c) {
+        kernel[i][j][c] = value;
+      }
+      sum += value;
     }
   }
 
